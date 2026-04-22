1177e004c8cbf6900ea41fbd6e2ebe519b515ae2
yassinebouamamaups-code <yassine.bouamama.ups@gmail.com>
2026-04-22 18:02:17 +0200

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// window.addEventListener("scroll", function () {
-//     const header = document.querySelector("header");
-
-//     if (window.scrollY > 50) {
-//         header.classList.add("scrolled");
-//     } else {
-//         header.classList.remove("scrolled");
-//     }
-// });
