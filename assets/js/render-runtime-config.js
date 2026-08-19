da70e3756826829b6d1b592c1f24443309a27234
Yassine Bouamama <yassine.bouamama.ups@gmail.com>
2026-08-19 22:14:49 +0200

    Point frontend catalog source to published Google CSV

--- assets/js/render-runtime-config.js
+++ assets/js/render-runtime-config.js
@@ -1,5 +1,5 @@
 window.RENDER_RUNTIME_CONFIG = {
-    catalogSourceUrl: "https://la-goutte-de-mer-paiements.onrender.com/api/catalog/source.csv"
+    catalogSourceUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT4yIHVQ_Zei5NVhGh2QiG-qp2MbMjMirLZocFA9g0f_MMMQl-K6oQgFlCjjL3NZS8oFGDz9kEroOGL/pub?gid=1960069975&single=true&output=csv"
 };
 
 window.PRODUCTS_SOURCE_URL = window.RENDER_RUNTIME_CONFIG.catalogSourceUrl;
--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-DEFAULT_CATALOG_URL="https://la-goutte-de-mer-paiements.onrender.com/api/catalog/source.csv"
+DEFAULT_CATALOG_URL="https://docs.google.com/spreadsheets/d/e/2PACX-1vT4yIHVQ_Zei5NVhGh2QiG-qp2MbMjMirLZocFA9g0f_MMMQl-K6oQgFlCjjL3NZS8oFGDz9kEroOGL/pub?gid=1960069975&single=true&output=csv"
 CATALOG_URL="${CATALOG_SOURCE_URL:-$DEFAULT_CATALOG_URL}"
 ESCAPED_CATALOG_URL=$(printf "%s" "$CATALOG_URL" | sed "s/'/'\\\\''/g")
 
